bfaab9ff600af075bb17bdbc603c2dbbe9f98d1b
Wowygrimm <andria95@hotmail.fr>
2019-06-24 14:07:08 +0100

    Correction of the gaze direction + json file

--- Eye-Detection/kinect/FaceLandmarksKinect/Kinect_Landmarks.py
+++ Eye-Detection/kinect/FaceLandmarksKinect/Kinect_Landmarks.py
@@ -9,6 +9,7 @@ from frame import Frame
 import face_alignment
 from skimage import io
 from rotate.rotation import Rotation3d
+import json
 
 """
 detector = dlib.get_frontal_face_detector()
@@ -17,6 +18,17 @@ predictor = dlib.shape_predictor('../../shape_predictor_68_face_landmarks.dat')
 
 """
 Functions
+"""
+
+"""
+Rotations
+"""
+def rotation_x(v, alpha):
+    R = np.array([[1, 0, 0], [0, np.cos(alpha), np.sin(alpha)], [0, -np.sin(alpha), np.cos(alpha)]])
+    return np.dot(R, v)
+
+"""
+Norm of a vector
 """
 def normv(v):
     return(v / np.linalg.norm(v))
@@ -100,10 +112,12 @@ if __name__ == '__main__':
 		if w[2] < 0:
 			w = w * (-1)
 
+		corrected_w = rotation_x(w, 0.3)
+
 		left_eye = (preds[45,:] + preds[42,:])//2
 		right_eye = (preds[39,:] + preds[36,:])//2
-		end_line_left = left_eye + 300 * w
-		end_line_right = right_eye + 300 * w
+		end_line_left = left_eye + 300 * corrected_w
+		end_line_right = right_eye + 300 * corrected_w
 		line_left = np.array([left_eye,end_line_left])
 		line_right = np.array([right_eye, end_line_right])
 
@@ -135,7 +149,7 @@ if __name__ == '__main__':
 		"""
 		Points of interest from 2D picture to 3D space coordinates
 		"""
-		# w,x,y,z are coordinates whereas w_d, x_d, y_d, z_d are the depth values of these coordinates
+		# w,x,y,z are coordinates on the depth frame whereas w_d, x_d, y_d, z_d are the depth values of these coordinates
 		w, x, y, z = np.array([preds[36,0]*scale[0], preds[36,1]*scale[1]]), np.array([preds[45,0]*scale[0], preds[45,1]*scale[1]]), np.array([preds[27,0]*scale[0], preds[27,1]*scale[1]]), np.array([preds[51,0]*scale[0], preds[51,1]*scale[1]])
 		w_d, x_d, y_d, z_d = frameDepth[int(w[1]), int(w[0])], frameDepth[int(x[1]), int(x[0])], frameDepth[int(y[1]), int(y[0])], frameDepth[int(z[1]), int(z[0])]
 
@@ -153,11 +167,15 @@ if __name__ == '__main__':
 
 		right_eye_s = face_land[12:15]
 
-
+		
 
 		x_s = normv(x_1 - x_0)
 		y_s = normv(y_1 - y_0)
-		z_s = np.cross(x, y)
+		z_s = np.cross(x_s, y_s)
+
+		#Correction of the director vector
+		z_s = rotation_x(z_s,0.3)
+		
 
 		if z_s[2] > 0:
 			z_s = z_s * (-1)
@@ -165,6 +183,14 @@ if __name__ == '__main__':
 		k = - right_eye_s[2] / (z_s[2])
 
 		cible = right_eye_s + k*z_s
+
+		data_point = {
+			"x": cible[0],
+			"y": cible[1],
+			"z": cible[2]
+		}
+		with open("cible.json", "w") as write_file:
+			json.dump(data_point, write_file)
 
 		print("Draw a point here:", cible)
 
@@ -178,7 +204,7 @@ if __name__ == '__main__':
 		
 
 		if not image is None:
-			cv2.imshow("Output-Keypoints",depth)
+			cv2.imshow("Output-Keypoints",image)
 
 		#np.savetxt("depth.csv", frameDepth, delimiter=",")
 
