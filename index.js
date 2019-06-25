8ede2173d4021f7f0fa2f1252311a1aa87056b52
Florian Guitton <f.guitton@imperial.ac.uk>
2019-06-25 12:10:58 +0100

    Fixing a broadcast function up

--- index.js
+++ index.js
@@ -4,6 +4,15 @@ const expressWs = require('express-ws')(app);
 const port = 3000;
 const aWss = expressWs.getWss('/');
 
+const broadcast = function (data) {
+    aWss.clients.forEach(function (client) {
+        client.send(JSON.stringify({
+            timestamp: (new Date()).getTime(),
+            ...data
+        }));
+    });
+}
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
@@ -11,15 +20,16 @@ app.get('/', function (req, res, next) {
     res.sendfile(__dirname + '/ws.html');
 });
 
+app.post('/', function (req, res, next) {
+    broadcast(req.body);
+});
+
 app.ws('/', function (ws, req) {
     ws.on('message', function (msg) {
         console.log(msg);
-        aWss.clients.forEach(function (client) {
-            client.send(JSON.stringify({
-                date: (new Date()).getTime(),
-                message: msg,
-            }));
-        });
+        broadcast({
+            message: msg,
+        })
     });
     console.log('socket', req.testing);
 });
