b53b67da23d283343896e43eb274ab6b193799f8
Florian Guitton <f.guitton@imperial.ac.uk>
2019-06-25 12:02:34 +0100

    Adding WebSocket Broadcast

--- index.js
+++ index.js
@@ -1,20 +1,28 @@
 const express = require('express');
-const port = 3000;
 const app = express();
+const expressWs = require('express-ws')(app);
+const port = 3000;
+const aWss = expressWs.getWss('/');
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-app.get('/gaze', function (req, res) {
-    console.log('Hello,');
-    res.json({
-        message: "World !"
+app.get('/', function (req, res, next) {
+    res.sendfile(__dirname + '/ws.html');
+});
+
+app.ws('/', function (ws, req) {
+    ws.on('message', function (msg) {
+        console.log(msg);
+        aWss.clients.forEach(function (client) {
+            client.send(JSON.stringify({
+                date: (new Date()).getTime(),
+                message: msg,
+            }));
+        });
     });
-})
-app.post('/gaze', function (req, res) {
-    console.log(req.body);
-    res.json(req.body);
-})
+    console.log('socket', req.testing);
+});
 
 app.listen(port, function () {
     console.log("Server is running on " + port + " port");
