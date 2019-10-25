1fe1dd24365390a9714bc45d6376c56328d1c0cd
Wowygrimm <andria95@hotmail.fr>
2019-10-25 17:40:59 +0100

    Introducing GDO geommetry

---
+++ Eye-Detection/kinect/FaceLandmarksKinect/geommetry.py
@@ -0,0 +1,9 @@
+import numpy as np
+import math
+
+kinect_position = (x, y, z)
+theta_k
+R_kinect = np.array([[cos(theta_k), sin(theta_k), 0], [-sin(theta_k), cos(theta_k), 0], [0, 0, 1]])
+
+theta_node_1 = 3*313*pi/(180*16)
+theta_node_2 = 313*pi/(180*4)
