da742d468654d74bbf392ab703e1d82792d5b3ec
Wowygrimm <andria95@hotmail.fr>
2019-11-11 11:25:41 +0000

    Fixing git problems

--- Eye-Detection/kinect/FaceLandmarksKinect/Floor_space.py
+++ Eye-Detection/kinect/FaceLandmarksKinect/Floor_space.py
@@ -12,36 +12,31 @@ import sys
 import math
 
 class Floor:
-
- # Gets the first detected floor plane from the body frame, freezing the kinect
-    # until it is available.
-    def _get_kinect_tilt(self, bodyFrame):
-
-        # Save floor plane as [a,b,c,d], where ax+by+cz+d=0
-        floorPlane = np.array([bodyFrame.floor_clip_plane.x,
-                                   bodyFrame.floor_clip_plane.y,
-                                   bodyFrame.floor_clip_plane.z,
-                                   bodyFrame.floor_clip_plane.w])
-		
-        return math.atan(floorPlane[2]/floorPlane[1])
         
     # Automatically connects to the Kinect V2.
 	# If autoFloorPlane is true, the Kinect grabs
     # the first available floor clip plane from the Kinect body.
     def __init__(self, bodyFrame, autoFloorPlane = True):
-        # Get a reference to the Kinect sensor
-        self.kinect = PyKinectRuntime.PyKinectRuntime(PyKinectV2.FrameSourceTypes_Color |
-                                                      PyKinectV2.FrameSourceTypes_Depth |
-                                                      PyKinectV2.FrameSourceTypes_Body)
+        self.floorPlane = np.array([bodyFrame.floor_clip_plane.x,
+                                   bodyFrame.floor_clip_plane.y,
+                                   bodyFrame.floor_clip_plane.z,
+                                   bodyFrame.floor_clip_plane.w])
         if autoFloorPlane:
-            self.kinect_tilt = self._get_kinect_tilt(bodyFrame)
+            self.kinect_tilt = math.atan(self.floorPlane[2]/self.floorPlane[1])
+            self.R = np.array([[1,0,0], [0, math.cos(self.kinect_tilt), math.sin(self.kinect_tilt)], [0, -math.sin(self.kinect_tilt), math.cos(self.kinect_tilt)]])
         else:
             # By default, the floor space is the same as the camera space
             print("We have a floor detection problem")
 
     def point_to_transform_space(self, point):
-        theta = self.kinect_tilt
-        R = np.array([[1,0,0], [0, math.cos(theta), math.sin(theta)], [0, -math.sin(theta), math.cos(theta)]])
-        return np.dot(R, point)
+        return np.dot(self.R, point)
 
-
+    def face_direction(self):
+        v = np.array([0,0,-1])
+        u = np.array([self.floorPlane[0], self.floorPlane[1], self.floorPlane[2]])
+        s = self.floorPlane[3]
+        vprime = 2*np.dot(u,v)*u + (s*s - np.dot(u,u))*v + 2*s*np.cross(u,v)
+        if (vprime[0] == 0 and vprime[1] == 0 and vprime[2] == 0):
+            return None
+        else:
+            return vprime
