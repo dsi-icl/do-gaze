cc8d38d99914500052b76b396fa80e4c6f779c3b
Andrianirina RAKOTOHARISOA <andrianirinarakotoharisoa@dyn3137-83.wlan.ic.ac.uk>
2020-01-22 14:29:20 +0000

    Deletion of useless files

--- Eye-Detection/kinect/FaceLandmarksKinect/frame.py
+++
@@ -1,8 +0,0 @@
-class Frame():
-	frameRGB = None
-	frameDepth = None
-	frameDepthQuantized = None
-	frameSkeleton = None
-	frame_num = 0
-	shoulder_orientation_euler = None
-	shoulder_orientation_quat = None
